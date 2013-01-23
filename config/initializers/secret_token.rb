ccf2623b81df6e62e2a364dcb4b9516cbfca10be
sokoban <sokoban@sokoban-ui-MacBook-Pro.local>
2013-01-23 09:44:48 +0900

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScannerByRails::Application.config.secret_token = '919cd6decfb417754a7c2a5d8687c3234804a3009328456d44987848a57886f8ee89aca942039753f8565bf451a2753f2764dc546d167665f92f4feb0e1c4c0f'
